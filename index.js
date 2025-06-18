561d2a569fe1ac975cfa1c4f8bbaed506112e127
Dusan Prica <prica.dusan16@gmail.com>
2025-06-19 01:11:14 +0200

    changed a lot :)

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const hamburgerMenu = document.querySelector('.hamburger_menu'); // Ispravljen selector
-    const navLinksContainer = document.querySelector('.nav_links_container'); // Ispravljen selector
+    const hamburgerMenu = document.querySelector('.hamburger_menu');
+    const navLinksContainer = document.querySelector('.nav_links_container');
     const navLinks = document.querySelectorAll('.nav_links_container nav ul li a');
 
     function toggleMenu() {
-        hamburgerMenu.classList.toggle('active');
-        navLinksContainer.classList.toggle('active');
+        if (hamburgerMenu) {
+            hamburgerMenu.classList.toggle('active');
+        }
+        if (navLinksContainer) {
+            navLinksContainer.classList.toggle('active');
+        }
     }
 
     if (hamburgerMenu) {
         hamburgerMenu.addEventListener('click', toggleMenu);
     }
-
     navLinks.forEach(link => {
         link.addEventListener('click', function(e) {
-            if (navLinksContainer.classList.contains('active')) {
+            if (navLinksContainer && navLinksContainer.classList.contains('active')) {
                 toggleMenu();
             }
-
             if (this.hash !== "") {
                 e.preventDefault();
 
-                const hash = this.hash;
-
-                const targetElement = document.querySelector(hash);
+                const targetElement = document.querySelector(this.hash);
 
                 if (targetElement) {
                     window.scrollTo({
